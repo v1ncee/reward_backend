2dada8d4b8936c08485dbb4122e0897e669a4c73
v1ncee <duchateau.vince@gmail.com>
2018-11-20 12:55:49 +0100

    final touches

--- models/reward.model.js
+++ models/reward.model.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-<<<<<<< HEAD
     name: { type: String, unique: true, required: true },
-=======
     title: { type: String, required: true },
     description: { type: String, required: true },
->>>>>>> 114baaab6ba667c4b532e3409c73596eb86c9b41
     points: { type: Number, default: 0 }
 });
 
